3f208c4ba8c9e3b93252fc5eef9e6262e8445043
MAGNIEN Sébastien <spam@magnien.info>
2014-06-24 23:12:47 +0200

    PEP8-isation de src/gui

--- src/gui/toolOptions.py
+++ src/gui/toolOptions.py
@@ -3,22 +3,21 @@
 
 from PySide import QtGui, QtCore
 
-class toolOptions(QtGui.QWidget) :
-  """Contient des widgets permettant de modifier les paramètres 
-  de l'objet sélectionné"""
-  def __init__(self) :
-    super(toolOptions, self).__init__()
-    
-    layout = QtGui.QVBoxLayout()
-    label = QtGui.QLabel(u"Inputs number")
-    layout.addWidget(label)
-    nInputs = QtGui.QLineEdit(self)
-    nInputs.setText('2')
-    layout.addWidget(nInputs)
-    self.setLayout(layout)
-   
-  @staticmethod
-  def updateOptions() : 
-    print 'toto'
 
+class toolOptions(QtGui.QWidget):
+    """Contient des widgets permettant de modifier les paramètres
+    de l'objet sélectionné"""
+    def __init__(self):
+        super(toolOptions, self).__init__()
 
+        layout = QtGui.QVBoxLayout()
+        label = QtGui.QLabel(u"Inputs number")
+        layout.addWidget(label)
+        nInputs = QtGui.QLineEdit(self)
+        nInputs.setText('2')
+        layout.addWidget(nInputs)
+        self.setLayout(layout)
+
+    @staticmethod
+    def updateOptions():
+        print 'toto'
