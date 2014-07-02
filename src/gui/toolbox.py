330cd9dbfb4e43762a745585e7c03d422312187b
MAGNIEN Sébastien <spam@magnien.info>
2014-07-02 11:57:02 +0200

    replacing toolbox by treewidget (was listwidget)

--- src/gui/toolbox.py
+++ src/gui/toolbox.py
@@ -4,7 +4,7 @@
 from PySide import QtGui, QtCore
 
 
-class ToolBox(QtGui.QListWidget):
+class ToolBox(QtGui.QTreeWidget):
     """A toolbox that contains logic gates and user circuits, for use
     in the main designer window.
     """
@@ -13,8 +13,21 @@ class ToolBox(QtGui.QListWidget):
 
     def __init__(self):
         super(ToolBox, self).__init__()
-        self.insertItems(0, ['And', 'Or', 'Nand', 'Nor', 'Not', 'Xor', 'Xnor'])
+        icon = QtGui.QIcon('gui/icons/AND.png')
         self.setDragEnabled(True)
+        self.setColumnCount(2)
+        gatesheader = QtGui.QTreeWidgetItem(self, [u'Basic Gates'])
+        gates = [
+            QtGui.QTreeWidgetItem(gatesheader, [name]) 
+            for name in ['And', 'Or', 'Nand', 'Nor', 'Not', 'Xor', 'Xnor']]
+        for gate in gates:
+            gate.setIcon(1, icon)
+        ioheader = QtGui.QTreeWidgetItem(self, [u'Inputs & Outputs'])
+        io = [
+            QtGui.QTreeWidgetItem(ioheader, [name]) 
+            for name in ['Input Pin', 'Output Pin']]
+        userheader = QtGui.QTreeWidgetItem(self, [u'User Circuits'])
+        self.insertTopLevelItems(0, [gatesheader, ioheader, userheader])
 
     def focusInEvent(self, event):
         self.clicked.emit(u'Ce panneau contient les objets que vous pouvez '
