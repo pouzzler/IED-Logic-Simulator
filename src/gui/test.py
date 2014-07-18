76265df5eb623158877e60f10a4e700ac401132f
MAGNIEN Sébastien <spam@magnien.info>
2014-07-18 23:47:32 +0200

    70 lignes pour dessinner l'interface; je pense qu'on peut aisément descendre de 400 lignes de settings.py à 150 ou 200 pour rajouter les connexions au modèle - voir test.py

--- src/gui/test.py
+++ src/gui/test.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-from PySide import QtGui
+from PySide import QtGui, QtCore
 
 
 class SettingsDialog(QtGui.QDialog):
-    """Manages clock speed, log functionality and GUI colors"""
+    """A dialog for choosing clock speed, log verbosity and GUI colors"""
 
     def __init__(self):
         super(SettingsDialog, self).__init__()
+        
+        self.setWindowTitle("Settings")
         
         logOutputs = QtGui.QGroupBox("Output logs to :")
         logOutputsLayout = QtGui.QVBoxLayout()
@@ -19,7 +21,25 @@ class SettingsDialog(QtGui.QDialog):
         logOutputsLayout.addWidget(logToStdout)
         logToFile = QtGui.QCheckBox("File")
         logOutputsLayout.addWidget(logToFile)
-        
+
+        logVerbosity = QtGui.QGroupBox("Select log verbosity :")
+        logVerbosityLayout = QtGui.QHBoxLayout()
+        logVerbosity.setLayout(logVerbosityLayout)
+        verbosityOptions = QtGui.QTreeWidget()   
+        for k, v in {   
+                'Item addition & removal' : [
+                    'I/O added', 'Circuit added', 'I/O removed',
+                    'Circuit removed', 'Detailed remove'],
+                'I/O (dis)connection' : [],
+                'I/O value modification' : [
+                    'Inputs', 'Outputs']}.items() :
+            node = QtGui.QTreeWidgetItem(verbosityOptions, [k])
+            node.setCheckState(0, QtCore.Qt.Checked)
+            for val in v :
+                item = QtGui.QTreeWidgetItem(node, [val])
+                item.setCheckState(0, QtCore.Qt.Checked)  
+        logVerbosityLayout.addWidget(verbosityOptions)
+
         clock = QtGui.QGroupBox("Clock :")
         clockLayout = QtGui.QHBoxLayout()
         clock.setLayout(clockLayout)
@@ -35,9 +55,10 @@ class SettingsDialog(QtGui.QDialog):
             QtGui.QLabel("Circuit background color"))
         appearanceLayout.addWidget(
             QtGui.QLabel("Logs background color"))
-        
-        layout = QtGui.QVBoxLayout(self)
-        layout.addWidget(logOutputs)
-        layout.addWidget(clock)
-        layout.addWidget(appearance)
+
+        layout = QtGui.QGridLayout(self)
+        layout.addWidget(logOutputs, 0, 0, 1, 1)
+        layout.addWidget(logVerbosity, 0, 1, 1, 1)
+        layout.addWidget(clock, 1, 0, 1, 2)
+        layout.addWidget(appearance, 2, 0, 1, 2)
         self.setLayout(layout)
