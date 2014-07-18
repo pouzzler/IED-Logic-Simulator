b26ab427d805ad1468cf66f694f71062853f08db
mathieufrh <mathieu.frh@live.de>
2014-07-18 11:22:34 +0200

    Create guilog.py

---
+++ src/gui/guilog.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+from PySide import QtGui, QtCore
+
+
+class LoggerTextEdit(QtGui.QTextEdit):
+    """A multiline text field that receives log messages."""
+
+    def __init__(self):
+        super(LoggerTextEdit, self).__init__()
+        self.pal = QtGui.QPalette()
+        #~ bgc = QtGui.QColor(0, 0, 0)
+        #~ self.pal.setColor(QtGui.QPalette.Base, bgc)
+        textc = QtGui.QColor(255, 255, 255)
+        self.pal.setColor(QtGui.QPalette.Text, textc)
+        self.setPalette(self.pal)
+
+    def write(self, text):
+        """Log handlers call a write() method."""
+        self.insertPlainText(text)
