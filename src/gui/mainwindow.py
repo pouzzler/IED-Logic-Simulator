f556d786a4dea53684b9e2a450a81367c51396d8
mathieu <mathieu.fourcroy@gmail.com>
2014-07-21 12:32:04 +0200

    test

--- src/gui/mainwindow.py
+++ src/gui/mainwindow.py
@@ -53,7 +53,7 @@ class MainWindow(QtGui.QMainWindow):
 
         self.settings = SettingsDialog(configFile)
         # TODO : il devrait décider sa géométrie tout seul
-        self.settings.setGeometry(100, 100, 600, 500)
+        self.settings.setGeometry(100, 100, 600, 550)
 
         fileMenu = QtGui.QMenu(u'File')
         fileMenu.addAction(u'Quit', self.close)
