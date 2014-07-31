dbb92f8c9e63055f3a6011dda743ac484466cb25
MAGNIEN Sébastien <spam@magnien.info>
2014-07-31 23:09:03 +0200

    small fixes

---
+++ src/gui/qtimports.py
@@ -0,0 +1,10 @@
+from PySide.QtGui import (
+    QAction, QApplication, QBrush, QCheckBox, QColor, QComboBox,
+    QDesktopWidget, QDialogButtonBox, QDockWidget, QDrag, QFileDialog,
+    QFont, QFrame, QGridLayout, QHBoxLayout, QGraphicsItem,
+    QGraphicsPathItem, QGraphicsScene, QGraphicsView, QIcon, QImage,
+    QInputDialog, QLabel, QLayout, QLineEdit, QMainWindow, QMenu,
+    QMessageBox, QPainter, QPainterPath, QPalette, QPen, QPixmap,
+    QPushButton, QRadioButton, qRgb, QScrollArea, QSizePolicy, QSplitter,
+    QStandardItemModel, QStyle, QTransform, QTextBrowser, QTextEdit,
+    QToolButton, QTreeWidget, QTreeWidgetItem, QWidget)
